8207ddd284e7cbc47d1dd5ec05a4084d02959c6d
Duy Vu <duyvu.mailbox@yahoo.com>
2016-05-17 20:39:30 -0400

    adding comments for HW0

--- HW/HW0.c
+++ HW/HW0.c
@@ -1,58 +1,87 @@
 #include <stdio.h>
 
+//function prototypes
 int add(int x, int y);
 int difference(int x, int y);
 void printBinary(int x);
 
-
+//adding 2 numbers
 int add(int x, int y) {
+    //if y is not 0
     while (y != 0) {
+        //carry contains the bits
+        //where the add operation need to carry to the more significant bit
+        //Ex: where 1 + 1
         int carry = x & y;
+        //x will then be the sum x + y
+        //But no carried bits has been added yet
         x = x ^ y;
+        //y will then be shift left then re-added to x in the next loop
+        //this is similar to normal plus operation
+        //where carried bits are added to the left bit next to it
         y = carry << 1;
     }
     return x;
 }
 
+//calculate the differece between x and y
 int difference(int x, int y) {
+    //the difference is |x - y|
+    //where it is either x + (-y) or y + (-x)
+    //whichever greater than 0
+    //subtraction x - y here is done by adding 1 to the flipped y (NOT y)
     if (add(x, add(~y, 1)) > 0)
         return add(x, add(~y, 1));
-    else 
-        return add(y, add(~x, 1)); 
+    else
+        return add(y, add(~x, 1));
 }
 
+//print the number in binary format
 void printBinary(int x) {
     int i;
+    //The size in bit of int is calculated by
+    //2^sizeof(int) => 2^4 = 32
     int size = 2<<sizeof(int);
+    //printing out the numbers
     for (i = size; i > 0;) {
+        //starting from 31
         i--;
-	int num = x>>i;
+        //num is x but right shift i numbers
+        //this help doing AND operation between x and 1
+        //starting form the left most bit
+        int num = x>>i;
         if (num & 1)
             printf("1");
         else
             printf("0");
-    }   
+    }
 }
 
+//main
 int main() {
     int x, y, sum, diff;
-   
+    //prompt for x and y
     printf("Enter x: ");
     scanf("%d",&x);
     printf("Enter y: ");
     scanf("%d", &y);
-    
+
+    //calculate sum and difference
     sum = add(x, y);
     diff = difference(x, y);
 
-    printf("a) The result of %d + %d is: \nBase 10: %d\nBase 2: ", x, y, sum); 
+    //print out sum
+    printf("a) The result of %d + %d is: \nBase 10: %d\nBase 2: ", x, y, sum);
     printBinary(sum);
 
+    //print out difference
     printf("\nb) The difference between %d and %d is: \nBase 10: %d\nBase 2: ", x, y, diff);
     printBinary(diff);
 
+    //format print for readability
     printf("\n\n");
     return 0;
 }
+
 
 
--- HW/HW0Q5.c
+++ HW/HW0Q5.c
@@ -1,49 +1,82 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+* This program is done in simple way since
+* this matrix is an even 3 x 3 matrix
+* Therefore using this for Ex 2 x 3 matrices will need changes in code
+*/
+
+//size of the matrix
 int matrixSize = 9;
+//each side of matrix
 int matrixSide = 3;
 
+//print out the matrix
 void printMatrix(int *matrix) {
     int i;
     printf("The matrix is: \n\t");
-
+    //the idea is:
+    //from the 1 dimentinal array that represent the matrix
+    //if the index of that number is divisible by 3 (which is the matrix size)
     for (i = 0; i < matrixSize; i++) {
-	if (i % 3 == 0 && i != 0) {
+        if (i % 3 == 0 && i != 0) {
             printf("\n\t");
-	}
-        printf("%d\t", *(matrix + i)); 
+        }
+        printf("%d\t", *(matrix + i));
     }
+
+    //print newlines for readability
     printf("\n\n");
 }
 
-
+//transpose the matrix using array index
 void indexTranspose(int matrix[matrixSize]) {
     int i;
-    
-    for (i = 0; i < matrixSize; i++) {
-	int row = i / matrixSide;
-        int column = i % matrixSide;
-        int index = row + column * matrixSide;
-	
-        if (index > i) {
-	    int temp = matrix[i];
-	    matrix[i] = matrix[index];
-	    matrix[index] = temp; 
-	}
-    }
+    //the idea is to calculate the index of the element to swap
 
-}
+    //by calculate the element index into row and column
 
+    //the index normally is: index = column + row * matrixSide
 
-void pointerTranspose(int *matrix) {
-    int i;
-
+    //But we trnasposing it by switching it to row + column * matrixSide
     for (i = 0; i < matrixSize; i++) {
         int row = i / matrixSide;
         int column = i % matrixSide;
         int index = row + column * matrixSide;
+        //since we goes through the whole list
 
+        //This if below make sure swapping once for each pair of indexcies
+        if (index > i) {
+            int temp = matrix[i];
+            matrix[i] = matrix[index];
+            matrix[index] = temp;
+        }
+    }
+
+}
+
+//transpose the matrix using pointer
+void pointerTranspose(int *matrix) {
+    int i;
+    //the idea is to calculate the index of the element to swap
+
+    //by calculate the element index into row and column
+
+    //the index normally is: index = column + row * matrixSide
+
+    //But we trnasposing it by switching it to row + column * matrixSide
+    for (i = 0; i < matrixSize; i++) {
+        int row = i / matrixSide;
+        int column = i % matrixSide;
+        int index = row + column * matrixSide;
+        //since we goes through the whole list
+
+        //This if below make sure swapping once for each pair of indexcies
+
+        //This is the only part
+
+        //where it is different from transpose by index above
         if (index > i) {
             int temp = *(matrix + i);
             *(matrix + i) = *(matrix + index);
@@ -54,23 +87,34 @@ void pointerTranspose(int *matrix) {
 }
 
 
+
+//main
 int main(void) {
     int *matrix = malloc(sizeof(int) * matrixSize);
     int i;
 
+
+    //prompt for user input
     for (i =0;i < matrixSize;i++) {
         printf("Please input a number:  ");
         scanf("%d", matrix + i);
     }
-    
+
+
+    //print the matrix
     printMatrix(matrix);
-   
+
+
+    //index transpose
     printf("a) Transpose using indexces\n");
     indexTranspose(matrix);
-    printMatrix(matrix); 
+    printMatrix(matrix);
 
+
+    //pointer transpose
     printf("b) Transpose using pointer\n");
     pointerTranspose(matrix);
     printMatrix(matrix);
 
 }
+
