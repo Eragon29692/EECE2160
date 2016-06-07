10be993f752c5491bb18db883d0476ab57b7f7ea
Duy Vu <duyvu.mailbox@yahoo.com>
2016-06-07 11:28:15 -0400

    HW2Q2

--- HW/HW2/Q2/main.cpp
+++ HW/HW2/Q2/main.cpp
@@ -1,5 +1,32 @@
 #include <iostream>
 #include <string>
-#include "Calculator.h"
+#include "Furniture.h"
+#include "Table.h"
+#include "Bed.h"
 using namespace std;
 
+int main() {
+	string tableName;
+	string woodType;
+	string bedName;
+	string size;
+	
+	cout << "Creating table...\n";
+	cout << "Enter name: "
+	cin >> tableName;
+	cout << "Enter wood type: "
+	cin >> woodType;
+	Table table(tableName, woodType);
+	
+		
+	cout << "Creating Bed...\n";
+	cout << "Enter name: "
+	cin >> bedName;
+	cout << "Enter bed size: "
+	cin >> size;
+	Bed Bed(bedName, size);
+	
+	cout << "\n\nPrinting objects ...\n\n"
+	
+	
+}
