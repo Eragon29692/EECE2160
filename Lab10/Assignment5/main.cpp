f7ad7522e710646ad089c3f600a2fafec87810af
Duy Vu <duyvu.mailbox@yahoo.com>
2016-06-26 22:01:55 -0400

    adding comment for lab 10 code

--- Lab10/Assignment5/main.cpp
+++ Lab10/Assignment5/main.cpp
@@ -7,16 +7,16 @@
 using namespace std;
 int main()
 {
+    //file descriptors to the axis and buttons events
     int fd1;
     int fd2;
-    //Accel
+    //Acceleration events
     fd1 = open("/dev/input/event0", O_RDONLY | O_NONBLOCK);
     if (fd1 == -1) {
         std::cerr << "Error: Could not open event file - forgot sudo?\n";
         exit(1);
     }
-    //Button
-    //open file descriptor
+    //Button events
     fd2 = open("/dev/input/event2", O_RDONLY | O_NONBLOCK);
     //check for error
     if (fd1 == -1) {
@@ -24,6 +24,7 @@ int main()
         exit(1);
     }
 
+    //set the servos initial angle
     RoboticArm robotic_arm;
     int angle0 = 90;
     int angle1 = 90;
@@ -31,9 +32,10 @@ int main()
     int angle3 = 90;
     int angle4 = 90;
 
-    //int angle4 = 90;
+    //slow down time
     int stime = 8000;
 
+    //counting until the slow down time for each button/axis
     int button103 = 0;
     int button108 = 0;
     int button105 = 0;
@@ -43,9 +45,11 @@ int main()
     int xTime = 0;
     int yTime = 0;
 
+    //delayed Acceleration values
     int xAccel = 0;
     int yAccel = 0;
 
+    //
     while (true)
     {
         // Read a packet of 32 bytes from Wiimote
@@ -54,13 +58,13 @@ int main()
         // Extract code (byte 10) and value (byte 12) from packet
         int code1 = buffer1[10];
         short acceleration = * (short *) (buffer1 + 12);
+        //read X axis value
         if (code1 == 3) {
             xAccel = acceleration;
-            //cout << "\nacc: " << xAccel << "  angle: " << angle0;
         }
+        //read y axis value
         if (code1 == 4) {
             yAccel = acceleration;
-            // cout << "\nacc: " << yAccel;
         }
 
 
@@ -71,41 +75,44 @@ int main()
         int value = buffer2[12];
 
 
-
-
-
+        //turn the remote left
         if (xAccel > 30) {
             xTime++;
+            //check for valid angle and slow down time
             if (angle0 < 179 && xTime > stime) {
                 angle0++;
                 xTime = 0;
             }
         }
+        //turn remote right
         if (xAccel < -50) {
             xTime++;
+            //check for valid angle and slow down time
             if (angle0 > 0 && xTime > stime) {
                 angle0--;
                 xTime = 0;
             }
         }
-
+        //turn remote up
         if (yAccel > -80) {
             yTime++;
+            //check for valid angle and slow down time
             if (angle1 > 0 && yTime > stime) {
                 angle1--;
                 yTime = 0;
             }
         }
+        //turn it down
         if (yAccel < -20) {
             yTime++;
+            //check for valid angle and slow down time
             if (angle1 < 179 && yTime > stime) {
                 angle1++;
                 yTime = 0;
             }
         }
 
-
-
+        //button up
         if (code2 == 103) {
             if (value == 0) {
                 button103 = 0;
@@ -119,7 +126,7 @@ int main()
             }
         }
 
-
+        //button down
         if (code2 == 108) {
             if (value == 0) {
                 button108 = 0;
@@ -133,7 +140,7 @@ int main()
             }
         }
 
-
+        //button right
         if (code2 == 105) {
             if (value == 0) {
                 button105 = 0;
@@ -147,7 +154,7 @@ int main()
             }
         }
 
-
+        //button left
         if (code2 == 106) {
             if (value == 0) {
                 button106 = 0;
@@ -161,7 +168,7 @@ int main()
             }
         }
 
-
+        //button trigger
         if (code2 == 49) {
             if (value == 0) {
                 button49 = 0;
@@ -175,6 +182,7 @@ int main()
             }
         }
 
+        //button A
         if (code2 == 48) {
             if (value == 0) {
                 button48 = 0;
@@ -188,16 +196,11 @@ int main()
             }
         }
 
-
-
-
-
+        //move the rbotic arm to the angle
         robotic_arm.MoveServo(3, angle3, 90);
         robotic_arm.MoveServo(0, angle0, 40);
         robotic_arm.MoveServo(1, angle1, 90);
         robotic_arm.MoveServo(2, angle2, 40);
         robotic_arm.MoveServo(4, angle4, 180);
-
-
     }
 }
